5c39bd1447c743d9de360ff00f0198d2d9990bab
Brendan McMahon <bernos@gmail.com>
2015-08-16 13:59:53 +1000

    Tidied up templates

--- scripts/process-templates.sh
+++ scripts/process-templates.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env sh
-TEMPLATES="one.template.txt
-two.template.txt
-three.template.txt"
+TEMPLATE_DIR=./templates
+OUTPUT_DIR=./output
 
-TOKENS="VAR_ONE:Enter a value for VAR_ONE
-VAR_TWO:Enter a value for VAR_TWO"
+TOKENS="
+VAR_ONE:Enter a value for VAR_ONE
+VAR_TWO:Enter a value for VAR_TWO
+"
 
 IFS=$(echo "\n\b")
 
+stty -echo
 for line in $TOKENS; do
     var=$(echo "$line" | cut -f 1 -d :)
     prompt=$(echo "$line" | cut -f 2- -d :)
-    echo -n "$var - $prompt: "
-    read $var
+    read -p "$var - $prompt: " $var
 done
+stty echo
 
-for filename in $TEMPLATES; do
-    dest=$(echo $filename | sed 's/\.template//g')
+rm -rf $OUTPUT_DIR
+cp -r $TEMPLATE_DIR/ $OUTPUT_DIR
+
+for filename in $(find $OUTPUT_DIR -type f); do
     content=$(eval "echo \"$(cat $filename)\"")
-    echo $content > $dest
+    echo $content > $filename
+    echo "Created file at $filename"
 done
